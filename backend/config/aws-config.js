fb4bd86ad47b8ecab72342b5e709131b66bc261e
Atharv Kulakrni <atharvkulkarni979@gmail.com>
2025-12-30 10:57:41 +0530

    push testing

---
+++ backend/config/aws-config.js
@@ -0,0 +1,9 @@
+const AWS = require("aws-sdk");
+
+AWS.config.update({ region: "eu-north-1" });
+
+const s3 = new AWS.S3();
+const S3_BUCKET = "atharvawsbucket4898";
+
+
+module.exports = { s3, S3_BUCKET };
